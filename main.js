beb761f1a2f1f072329ea765e18e51aadc472f6b
cavedave2 <sflanag5@gmail.com>
2021-01-12 16:22:19 +0000

    task1

--- main.js
+++ main.js
@@ -23,8 +23,33 @@ const things = [
   "bowl",
 ];
 
+//go into array
+//loop over array
+//keep items that don't start with a vowel (aeiou)
+//create a new array with these filtered values .filter
+
+//write function
+
+let answer =things.filter(
+  function (word) {
+    if(word.charAt(0) === "a"||
+    word.charAt(0) === "e"||
+    word.charAt(0) === "i"||
+    word.charAt(0) === "o"||
+    word.charAt(0) === "u"){
+      return false
+    } else {
+      return true}
+  })
+
+console.log(answer)
+
+
+
 //👉 Your code here!
 
 //-----Task 2:-----
+
+
 
 //👉 Your code here!
